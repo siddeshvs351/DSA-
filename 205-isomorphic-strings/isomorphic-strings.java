a2a047524a8a15a1085ca3015e88f63f56314738
Siddesh V S <siddeshvs351@gmail.com>
2026-08-09 21:20:35 +0530

    Time: 10 ms (72.38%) | Memory: 43.8 MB (74.61%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map=new HashMap<Character,Character>();
+        HashSet<Character> set=new HashSet<Character>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        for(int i=0;i<s.length();i++){
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+
+            if(map.containsKey(c1)){
+                if(map.get(c1)!=c2){
+                    return false;
+                }
+            }
+            else{
+                if(set.contains(c2)){
+                    return false;
+                }
+                map.put(c1,c2);
+                set.add(c2);
+            }
+
+        }
+        return true;
+        
+    }
+}
